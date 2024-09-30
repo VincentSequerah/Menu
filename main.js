3a73dbf0acd7894d3d753244bb93aa7d259b077a
VincentSequerah <v1jnicholas@gmail.com>
2024-09-30 23:33:38 +0700

    finished js.

--- main.js
+++ main.js
@@ -37,66 +37,58 @@ const menu =[
 
 
 },
-{
-    name:'Bread',
-    price: 10.999,
-    img:'./images/bread.jfif',
-    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia quas corrupti aperiam qui quos, ea ipsum nihil enim dolor corporis.',
-    category:'breakfast',
 
-
-},
 {
-    name:'Butter',
+    name:'Hawaiian',
     price: 3.99,
-    img:'./Images/Butter.webp',
+    img:'Images/Hawaiian.jpg',
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia quas corrupti aperiam qui quos, ea ipsum nihil enim dolor corporis.',
-    category:'breakfast',
+    category:'pizza',
 
 
 },
 {
-    name:'Mango',
+    name:'Sundae',
     price: 9.99,
-    img:'Images/Mango.jfif',
+    img:'Images/Sundae.jfif',
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia quas corrupti aperiam qui quos, ea ipsum nihil enim dolor corporis.',
-    category:'breakfast',
+    category:'dessert',
 
 
 },
 {
-    name:'Milk',
+    name:'Strawberry Milkshake',
     price: 0.99,
-    img:'Images/Milk.jfif',
+    img:'Images/strawberrymilkshake.jpg',
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia quas corrupti aperiam qui quos, ea ipsum nihil enim dolor corporis.',
     category:'drink',
 
 
 },
 {
-    name:'Bread',
+    name:'Vindaloo',
     price: 10.999,
-    img:'./images/bread.jfif',
+    img:'./images/Vindaloo.jfif',
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia quas corrupti aperiam qui quos, ea ipsum nihil enim dolor corporis.',
-    category:'breakfast',
+    category:'curry',
 
 
 },
 {
-    name:'Butter',
+    name:'Nachos',
     price: 3.99,
-    img:'./Images/Butter.webp',
+    img:'./Images/Nachos.jpg',
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia quas corrupti aperiam qui quos, ea ipsum nihil enim dolor corporis.',
-    category:'breakfast',
+    category:'starters',
 
 
 },
 {
-    name:'Mango',
+    name:'Key Lime pie',
     price: 9.99,
-    img:'Images/Mango.jfif',
+    img:'Images/LimePie.jpg',
     description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia quas corrupti aperiam qui quos, ea ipsum nihil enim dolor corporis.',
-    category:'breakfast',
+    category:'dessert',
 
 
 },
@@ -115,20 +107,22 @@ const menu =[
 
 
 
-let selection = 'breakfast';
+let selection;
 let selectedItems = menu;
 
 //filtering Menu for food types depending on button pressed
-function myFunction(x){
+function filterFood(x){
     selection=x;
 
     selectedItems = menu.filter(foodType);
  
     function foodType(item){
+        if (selection=='all'){
+            return selectedItems = menu;         
+        }
         return  item.category === selection;
     } ;
 
-    console.log(selectedItems);
 
 
     
@@ -144,37 +138,21 @@ function myFunction(x){
 
 
 const display = document.querySelector(".display");   //get display section
-
-window.addEventListener("DOMContentLoaded", function () {       //on page loading,  create a new array by calling a function on each original array element 
-  let displayMenu = selectedItems.map(function (item) {
-
-    //populate displayMenu with the HTML below, with details filled in from menu array
-
-    return `<article>
-            <header class="name">
-                <h3>${item.name}</h3>
-                <h3 class="price">${item.price}</h3>
-            </header>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, enim? Vero adipisci perferendis ea sit, esse perspiciatis maxime corrupti magnam!</p>
-            <img src=${item.img} alt=${item.name} title=${item.name}>
-        </article>`;
-
-        
-
-
-  });
-
-  
-  displayMenu = displayMenu.join('');   //returns array (displayMenu) as a string
-
-
-  display.innerHTML = displayMenu;
-});
+window.addEventListener("DOMContentLoaded", refreshDisplay);     //on page loading,  create a new array by calling a function on each original array element 
 
 
 
 
-window.addEventListener('click', function () {       //on page loading,  create a new array by calling a function on each original array element 
+window.addEventListener('click', refreshDisplay);    //when a button is clicked, refresh
+
+
+
+
+
+
+
+function refreshDisplay()
+{      
     let displayMenu = selectedItems.map(function (item) {
   
       //populate displayMenu with the HTML below, with details filled in from menu array
@@ -194,8 +172,8 @@ window.addEventListener('click', function () {       //on page loading,  create 
     });
   
     
-    displayMenu = displayMenu.join('');   //returns array (displayMenu) as a string
+    displayMenu = displayMenu.join('');   //returns array (displayMenu) as a string adding each menu item that was selected together
   
   
-    display.innerHTML = displayMenu;
-  });
+    display.innerHTML = displayMenu;    //write value of displayMenu(item that have been selected) to the display
+  };
